78fcec0d07a298338ebbc366f4b19353948432f2
Davon Belvin <davonbelvin@gmail.com>
2021-11-15 15:07:27 -0800

    button click successfully shows about me tile need to normalize sizes

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -2,7 +2,20 @@ import React from "react";
 
 const AboutMe = (props) => {
   return (
-    <p>Highly motivated, philosopher, classical guitarist, and family man</p>
+    <div className="about-me-tile">
+      <h1>
+        Highly motivated, philosopher, classical guitarist, and family man.
+      </h1>
+      <p>
+        I love learning new things. Similarly, I enjoy seeking new perspectives.
+        My ultimate goal is to be a sage, master in an art, and to live until I
+        am 121. I believe the journey is more important than the destination. I
+        love thinking about the fundamental nature of reality, and
+        consciousness. I hope I live long enough to witness the technological
+        singularity. Maybe I will have some part in creating the AI that gets us
+        there.
+      </p>
+    </div>
   );
 };
 
