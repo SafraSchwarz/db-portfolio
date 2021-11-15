faedceec3153f7b2c1c0966d24ddcb8a2b0be7ee
Davon Belvin <davonbelvin@gmail.com>
2021-11-14 18:20:33 -0800

    started portfolio with heavy css

---
+++ src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LandingPage = (props) => {
+  return (
+    <div className="landing-page">
+      <h1 className="my-name">Davon Belvin</h1>
+      <p className="my-title">Front End Developer</p>
+    </div>
+  );
+};
+
+export default LandingPage;
